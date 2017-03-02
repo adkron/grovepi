a4fd9e287f4dae6e1c6aa440a63b2786c6a2a50c
Frank Hunleth <fhunleth@troodon-software.com>
2017-03-01 20:59:50 -0500

    Add note about checking whether the GrovePi works

--- lib/grovepi.ex
+++ lib/grovepi.ex
@@ -3,6 +3,14 @@ defmodule GrovePi do
   Low-level interface for sending raw requests and receiving responses from a
   GrovePi hat. Create one of these first and then use one of the other GrovePi
   modules for interacting with a connected sensor, light, or actuator.
+
+  To check that your GrovePi hardware is working, try this:
+
+  iex> {:ok, pid}=GrovePi.start_link()
+  {:ok, #PID<0.212.0>}
+  iex> GrovePi.firmware_version(pid)
+  "1.2.2"
+
   """
 
   @grovepi_address 0x04
