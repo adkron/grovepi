8aecea4ea8862a6205b24a3c04557619be44cb20
Amos L King <amos.l.king@gmail.com>
2017-03-21 12:23:39 -0500

    Add top level testing of registration and messages
    Amos King @adkron <amos@binarynoggin.com>

---
+++ test/buttons_test.exs
@@ -0,0 +1,23 @@
+defmodule GrovePi.ButtonsTest do
+  use ExUnit.Case
+
+  test "registering for a pressed event receives presses" do
+    pin = 5
+    GrovePi.Buttons.start_link(self)
+    GrovePi.Buttons.register({:pressed, pin})
+
+    GrovePi.Buttons.notify_change(pin, 0, 1)
+
+    assert_receive {:pressed, pin}
+  end
+
+  test "registering for a released event receives presses" do
+    pin = 5
+    GrovePi.Buttons.start_link(self)
+    GrovePi.Buttons.register({:pressed, pin})
+
+    GrovePi.Buttons.notify_change(pin, 1, 0)
+
+    assert_receive {:released, pin}
+  end
+end
