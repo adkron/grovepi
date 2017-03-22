296b98af3238a7388db0796708701783524b12cc
Amos L King <amos.l.king@gmail.com>
2017-03-21 21:09:17 -0500

    Mock I2C for testing
    I created a mock of I2C that allows the setting of a state for a
    response. I think that the next step is to make it store any data we
    want to read. For now it is very focused on reading a button.
    
    Amos King @adkron <amos@binarynoggin.com>

--- test/buttons_test.exs
+++ test/buttons_test.exs
@@ -1,20 +1,36 @@
 defmodule GrovePi.ButtonsTest do
   use ExUnit.Case, async: true
+  @pressed 1
+  @released 0
 
-  test "registering for a pressed event receives presses" do
+  setup do
+    :timer.sleep 100
+    {:ok, grove_pid} = GrovePi.start_link
+    :ok = GrovePi.Buttons.start_link(grove_pid)
+
+    {:ok, [grove: grove_pid]}
+  end
+
+  test "registering for a pressed event receives pressed messages",
+  %{grove: grove} do
     pin = 5
-    GrovePi.Buttons.start_link(self)
     GrovePi.Buttons.register({:pressed, pin})
+
+    GrovePi.I2C.set_level(grove, @pressed)
 
     GrovePi.Buttons.notify_change(pin, {0, 1})
 
     assert_receive {:pressed, pin}
   end
 
-  test "registering for a released event receives presses" do
+  test "registering for a released event receives released messages",
+  %{grove: grove} do
     pin = 5
-    GrovePi.Buttons.start_link(self)
     GrovePi.Buttons.register({:released, pin})
+
+    GrovePi.I2C.set_level(grove, @pressed)
+    :timer.sleep 100
+    GrovePi.I2C.set_level(grove, @released)
 
     GrovePi.Buttons.notify_change(pin, {1, 0})
 
