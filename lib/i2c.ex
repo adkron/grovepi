2dcd9098c1d2547833cd57ff0ddcf53bf9d484ff
Amos L King <amos.l.king@gmail.com>
2017-03-22 19:50:28 -0500

    Update tests to use i2c stand in
    The stand in was being bypassed and it was not returning binary.
    
    Amos King @adkron <amos@binarynoggin.com>

--- lib/i2c.ex
+++ lib/i2c.ex
@@ -2,40 +2,49 @@ defmodule GrovePi.I2C do
   use GenServer
 
   @type i2c_address :: 0..127
-  @low 0
-  @high 1
 
   defmodule State do
-    defstruct level: @low, current_pin: nil
+    defstruct output_messages: [], input_messages: []
   end
 
   @spec start_link(binary, i2c_address, [term]) :: {:ok, pid}
-  def start_link(_devname, _address, opts \\ []) do
+  def start_link(_devname, _address, _opts \\ []) do
     GenServer.start_link(__MODULE__, %State{})
   end
 
-  def set_level(pid, level) do
-    GenServer.call(pid, {:set_level, level})
+  def add_responses(pid, messages) do
+    GenServer.call(pid, {:add_responses, messages})
+  end
+
+  def add_response(pid, message) do
+    GenServer.call(pid, {:add_responses, [message]})
   end
 
   @spec write(pid, binary) :: :ok
-  def write(pid, <<1, pin, 0, 0>>) do
-    GenServer.call(pid, {:write, pin})
+  def write(pid, <<1, _pin, 0, 0>> = message) do
+    GenServer.call(pid, {:write, message})
   end
 
-  def read(pid, 1) do
-    GenServer.call(pid, {:read, 1})
+  def read(pid, len) do
+    GenServer.call(pid, {:read, len})
   end
 
-  def handle_call({:write, pin}, _from, state) do
-    {:reply, :ok, %{state | current_pin: pin}}
+  def handle_call({:write, message}, _from, state) do
+    {:reply, :ok, %{state | input_messages: [message, state.input_messages]}}
   end
 
-  def handle_call({:read,  1}, _from, state) do
-    {:reply, state.level, %{state | current_pin: nil}}
+  def handle_call({:read,  _len}, _from, %State{output_messages: []} = state) do
+    {:reply, <<0>>, state}
   end
 
-  def handle_call({:set_level, level}, _from, state) do
-    {:reply, :ok, %{state | level: level}}
+  def handle_call({:read,  _len}, _from, %State{output_messages: output_messages} = state) do
+    [message | rest_output_messages] = output_messages
+    {:reply, message, %{state | output_messages: rest_output_messages}}
   end
+
+  def handle_call({:add_responses, messages}, _from, state) do
+    {:reply, :ok, %{state | output_messages: state.output_messages ++ messages}}
+  end
+
+  def write_device(_,_,_), do: :ok
 end
