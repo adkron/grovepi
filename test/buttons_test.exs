35095b6c4c49e49ac2937657a5db92a6601153ed
Amos L King <amos.l.king@gmail.com>
2017-03-22 19:50:28 -0500

    Update tests to use i2c stand in
    The stand in was being bypassed and it was not returning binary.
    
    Amos King @adkron <amos@binarynoggin.com>

--- test/buttons_test.exs
+++ test/buttons_test.exs
@@ -1,7 +1,7 @@
 defmodule GrovePi.ButtonsTest do
   use ExUnit.Case, async: true
-  @pressed 1
-  @released 0
+  @pressed <<1>>
+  @released <<0>>
 
   setup do
     :timer.sleep 100
@@ -14,26 +14,20 @@ defmodule GrovePi.ButtonsTest do
   test "registering for a pressed event receives pressed messages",
   %{grove: grove} do
     pin = 5
+    GrovePi.Buttons.add(pin)
     GrovePi.Buttons.register({:pressed, pin})
+    GrovePi.I2C.add_response(grove, @pressed)
 
-    GrovePi.I2C.set_level(grove, @pressed)
-
-    GrovePi.Buttons.notify_change(pin, {0, 1})
-
-    assert_receive {:pressed, pin}
+    assert_receive {:pressed, pin}, 300
   end
 
   test "registering for a released event receives released messages",
   %{grove: grove} do
     pin = 5
+    GrovePi.Buttons.add(pin)
     GrovePi.Buttons.register({:released, pin})
+    GrovePi.I2C.add_responses(grove, [@pressed, @released])
 
-    GrovePi.I2C.set_level(grove, @pressed)
-    :timer.sleep 100
-    GrovePi.I2C.set_level(grove, @released)
-
-    GrovePi.Buttons.notify_change(pin, {1, 0})
-
-    assert_receive {:released, pin}
+    assert_receive {:released, pin}, 300
   end
 end
