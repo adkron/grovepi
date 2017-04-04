0d6d42c8a463c9379b83ca3d7729a63f95079f04
Amos L King <amos.l.king@gmail.com>
2017-04-04 12:48:09 -0500

    Move DHT to a process
    We are moving towards a consitent interface that each type of sensor has
    a consitent interface. You start the sensor with the pin and then
    interact with it that way too.
    
    Amos King @adkron <amos@binarynoggin.com>

--- lib/grovepi/dht.ex
+++ lib/grovepi/dht.ex
@@ -5,7 +5,7 @@ defmodule GrovePi.DHT do
   Example use:
 
   ```
-  iex> {:ok, pid}=GrovePi.start_link
+  iex> {:ok, button}=GrovePi.DHT.start_link(3)
   {:ok, #PID<0.199.0>}
   iex> GrovePi.DHT.read_temp_and_humidity(pid, 2)
   {23.0, 40.0}
@@ -13,10 +13,31 @@ defmodule GrovePi.DHT do
 
   """
 
+  alias GrovePi.Utils
+
+  defmodule State do
+    @moduledoc false
+    defstruct [:pin]
+  end
+
+  @spec start_link(GrovePi.pin) :: Supervisor.on_start
+  def start_link(pin, opts \\ []) do
+    opts = Keyword.put(opts, :name, Utils.pin_name(pin))
+    GenServer.start_link(__MODULE__, [pin], opts)
+  end
+
+  def init([pin]) do
+    {:ok, %State{pin: pin}}
+  end
+
   def read_temp_and_humidity(pin, module_type \\ 0) do
-    :ok = GrovePi.Board.send_request(<<40, pin, module_type, 0>>)
+    GenServer.call(Utils.pin_name(pin), {:read_temp_and_humidity, module_type})
+  end
+
+  def handle_call({:read_temp_and_humidity, module_type}, _from, state) do
+    :ok = GrovePi.Board.send_request(<<40, state.pin, module_type, 0>>)
     <<_, temp::little-float-size(32), humidity::little-float-size(32)>> =
       GrovePi.Board.get_response(9)
-    {temp, humidity}
+      {:reply, {temp, humidity}, state}
   end
 end
---
+++ test/dht_test.exs
@@ -0,0 +1,23 @@
+defmodule GrovePi.DHTTest do
+  use ExUnit.Case
+  @pin 5
+
+  setup do
+    {:ok, _} = GrovePi.DHT.start_link(@pin)
+
+    GrovePi.I2C.reset(GrovePi.Board)
+
+    :ok
+  end
+
+  test "gets temp and humidity" do
+    temp = 20.0
+    humidity = 10.0
+
+    GrovePi.I2C.add_response(GrovePi.Board, <<1, temp::little-float-size(32), humidity::little-float-size(32)>>)
+
+
+    assert {temp, humidity} == GrovePi.DHT.read_temp_and_humidity(@pin)
+    assert <<40, @pin, 0, 0>> == GrovePi.I2C.get_last_write(GrovePi.Board)
+  end
+end
