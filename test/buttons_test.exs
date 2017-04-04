9abccaf550c8228201438b3f0bf6ea19b27c8ca4
Amos L King <amos.l.king@gmail.com>
2017-04-03 23:00:53 -0500

    Remove test setup in favor of module variable
    Since the pin is used in the entire file and we use
    the exact same pin I moved it to a module var. I also
    removed some duplicated pieces.
    
    Amos King @adkron <amos@binarynoggin.com>

--- test/buttons_test.exs
+++ test/buttons_test.exs
@@ -2,45 +2,41 @@ defmodule GrovePi.ButtonsTest do
   use ExUnit.Case
   @pressed <<1>>
   @released <<0>>
+  @pin 5
 
   setup do
-    pin = 5
-    {:ok, _} = GrovePi.Button.start_link(pin)
+    {:ok, _} = GrovePi.Button.start_link(@pin)
 
     GrovePi.I2C.reset(GrovePi.Board)
 
-    {:ok, [pin: pin]}
+    :ok
   end
 
-  test "registering for a pressed event receives pressed messages",
-  %{pin: pin} do
-    GrovePi.Button.subscribe(pin, :pressed)
+  test "registering for a pressed event receives pressed messages" do
+    GrovePi.Button.subscribe(@pin, :pressed)
     GrovePi.I2C.add_response(GrovePi.Board, @pressed)
 
-    assert_receive {^pin, :pressed}, 300
+    assert_receive {@pin, :pressed}, 300
   end
 
-  test "registering for a released event receives released messages",
-  %{pin: pin} do
-    GrovePi.Button.subscribe(pin, :released)
+  test "registering for a released event receives released messages" do
+    GrovePi.Button.subscribe(@pin, :released)
     GrovePi.I2C.add_responses(GrovePi.Board, [@pressed, @released])
 
-    assert_receive {^pin, :released}, 300
+    assert_receive {@pin, :released}, 300
   end
 
   @tag :capture_log
-  test "recovers from I2C error",
-  %{pin: pin} do
-    GrovePi.Button.subscribe(pin, :released)
-    GrovePi.I2C.add_responses(GrovePi.Board, [{:error, :i2c_write_failed}])
+  test "recovers from I2C error" do
+    GrovePi.Button.subscribe(@pin, :released)
+    GrovePi.I2C.add_responses(GrovePi.Board, [
+                                {:error, :i2c_write_failed},
+                                @pressed,
+                                @released,
+                              ])
 
     Process.sleep 100
 
-    GrovePi.I2C.add_responses(GrovePi.Board, [@pressed, @released])
-    assert_receive {^pin, :released}, 300
-  end
-
-  def resend(pid, argument) do
-    send(pid, {:called_with, argument})
+    assert_receive {@pin, :released}, 300
   end
 end
--- test/buzzer_test.exs
+++ test/buzzer_test.exs
@@ -2,33 +2,31 @@ defmodule GrovePi.BuzzerTest do
   use ExUnit.Case
   @on 1
   @off 0
+  @pin 5
 
   setup do
-    pin = 5
-    {:ok, _} = GrovePi.Buzzer.start_link(pin)
+    {:ok, _} = GrovePi.Buzzer.start_link(@pin)
 
     GrovePi.I2C.reset(GrovePi.Board)
 
-    {:ok, [pin: pin]}
+    :ok
   end
 
-  test "buzzes for one second by default",
-  %{pin: pin} do
-    GrovePi.Buzzer.buzz(pin)
-    Process.sleep 1100
-    {off_time, <<2, ^pin, @off, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
-    {on_time, <<2, ^pin, @on, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
+  test "buzzes for one second by default" do
+    GrovePi.Buzzer.buzz(@pin)
+    Process.sleep 1010
+    {off_time, <<2, @pin, @off, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
+    {on_time, <<2, @pin, @on, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
 
     assert_in_delta (off_time - on_time), 1000, 10
   end
 
-  test "buzzes for time specified",
-  %{pin: pin} do
+  test "buzzes for time specified" do
     interval = 100
-    GrovePi.Buzzer.buzz(pin, interval)
+    GrovePi.Buzzer.buzz(@pin, interval)
     Process.sleep interval + 10
-    {off_time, <<2, ^pin, @off, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
-    {on_time, <<2, ^pin, @on, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
+    {off_time, <<2, @pin, @off, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
+    {on_time, <<2, @pin, @on, 0>>} = GrovePi.I2C.get_last_write(GrovePi.Board, include_time: true)
 
     assert_in_delta (off_time - on_time), interval, 10
   end
