0c9e450b2ac007c34303b5f391cfe6ffa16ebbf8
Todd Pickell <tapickell@gmail.com>
2017-04-14 11:30:30 -0500

    Use generated prefix to avoid async issues in test
    This uses a prefix generated from a timestamp in the
    test setup to isolate the supervision trees for each test.
    This allows us to remove the sleep call in setup.
    This will need to be replicated to the other tests
    and refactored to pull out common code into a helper module
    
    Todd Pickell <tapickell@gmail.com> @tapickell
    Amos King <amos@binarynoggin.com> @adkron

--- test/button_test.exs
+++ test/button_test.exs
@@ -3,70 +3,81 @@ defmodule GrovePi.ButtonTest do
   @pressed <<1>>
   @released <<0>>
   @pin 5
-  @prefix __MODULE__
-  @board GrovePi.Board.i2c_name(__MODULE__)
+  @moduletag report: [:prefix, :board, :poll_interval]
 
-  def start_button(poll_interval \\ 1) do
-    Process.sleep 10
-
-    with {:ok, _} <- GrovePi.Supervisor.start_link(0x40, @prefix),
+  def start_button(prefix, poll_interval) do
+    with {:ok, _} <- GrovePi.Supervisor.start_link(0x40, prefix),
          {:ok, _} <- GrovePi.Button.start_link(@pin,
-                                          poll_interval: poll_interval,
-                                          prefix: @prefix,
+                                          poll_unterval: poll_interval,
+                                          prefix: prefix,
                                         ),
     do: :ok
   end
 
+  def setup_sub(prefix, action) do
+    GrovePi.Button.subscribe(@pin, action, prefix)
+  end
+
+  def setup_resp(board, actions) do
+    GrovePi.I2C.add_responses(board, actions)
+  end
+
   describe "default button" do
-    setup do
-      start_button()
+    setup tags do
+      prefix = Time.to_string(Time.utc_now)
+      board = GrovePi.Board.i2c_name(prefix)
+      poll_interval = Map.get(tags, :poll_interval, 1)
 
-      GrovePi.I2C.reset(@board)
+      start_button(prefix, poll_interval)
 
-      :ok
+      GrovePi.I2C.reset(board)
+
+      {:ok, [prefix: prefix, board: board]}
     end
 
-    test "registering for a pressed event receives pressed messages" do
-      GrovePi.Button.subscribe(@pin, :pressed, @prefix)
-      GrovePi.I2C.add_response(@board, @pressed)
+    test "registering for a pressed event receives pressed messages",
+      %{prefix: prefix, board: board} do
+      setup_sub(prefix, :pressed)
+      setup_resp(board, [@pressed])
 
       assert_receive {@pin, :pressed}, 300
     end
 
-    test "registering for a released event receives released messages" do
-      GrovePi.Button.subscribe(@pin, :released, @prefix)
-      GrovePi.I2C.add_responses(@board, [@pressed, @released])
+    test "registering for a released event receives released messages",
+      %{prefix: prefix, board: board} do
+      setup_sub(prefix, :released)
+      setup_resp(board, [@pressed, @released])
 
       assert_receive {@pin, :released}, 300
     end
 
     @tag :capture_log
-    test "recovers from I2C error" do
-      GrovePi.Button.subscribe(@pin, :released, @prefix)
-      GrovePi.I2C.add_responses(@board, [
-                                  {:error, :i2c_write_failed},
-                                  @pressed,
-                                  @released,
-                                ])
+    test "recovers from I2C error",
+      %{prefix: prefix, board: board} do
+      setup_sub(prefix, :released)
+      setup_resp(board, [
+                  {:error, :i2c_write_failed},
+                  @pressed,
+                  @released,
+                ])
 
       assert_receive {@pin, :released}, 300
     end
-  end
 
-  test "reading notifies subscribers" do
-    start_button(1000000)
+    @tag poll_interval: 1000000
+    test "reading notifies subscribers",
+      %{prefix: prefix, board: board} do
+      setup_sub(prefix, :released)
+      setup_sub(prefix, :pressed)
+      setup_resp(board, [@pressed, @released, @pressed])
 
-    GrovePi.I2C.reset(@board)
-    GrovePi.Button.subscribe(@pin, :released, @prefix)
-    GrovePi.Button.subscribe(@pin, :pressed, @prefix)
-    GrovePi.I2C.add_responses(@board, [@pressed, @released, @pressed])
+      GrovePi.Button.read(@pin, prefix)
 
-    GrovePi.Button.read(@pin, @prefix)
+      assert_receive {@pin, :pressed}, 10
 
-    assert_receive {@pin, :pressed}, 10
+      GrovePi.Button.read(@pin, prefix)
 
-    GrovePi.Button.read(@pin, @prefix)
-
-    assert_receive {@pin, :released}, 10
+      assert_receive {@pin, :released}, 10
+    end
   end
 end
