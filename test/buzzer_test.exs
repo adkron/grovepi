d1e978b4651a726aa15f4de5c5b9196fbdc658ad
Todd Pickell <tapickell@gmail.com>
2017-04-14 18:04:20 -0500

    Converts all tests to using new style
    This gives us better setup with prefix and board so
    tests can run async without already started errors.
    
    Todd Pickell <tapickell@gmail.com> @tapickell
    Amos King <amos@binarynoggin.com> @adkron

--- test/buzzer_test.exs
+++ test/buzzer_test.exs
@@ -3,40 +3,41 @@ defmodule GrovePi.BuzzerTest do
   @on 1
   @off 0
   @pin 5
-  @prefix __MODULE__
-  @board GrovePi.Board.i2c_name(__MODULE__)
 
-  def start_buzzer() do
-    Process.sleep 10
-
-    with {:ok, _} <- GrovePi.Supervisor.start_link(0x40, @prefix),
-         {:ok, _} = GrovePi.Buzzer.start_link(@pin, prefix: @prefix),
+  def start_buzzer(prefix) do
+    with {:ok, _} <- GrovePi.Supervisor.start_link(0x40, prefix),
+         {:ok, _} = GrovePi.Buzzer.start_link(@pin, prefix: prefix),
     do: :ok
   end
 
   setup do
-    start_buzzer()
+    prefix = String.to_atom(Time.to_string(Time.utc_now))
+    board = GrovePi.Board.i2c_name(prefix)
 
-    GrovePi.I2C.reset(@board)
+    start_buzzer(prefix)
 
-    :ok
+    GrovePi.I2C.reset(board)
+
+    {:ok, [prefix: prefix, board: board]}
   end
 
-  test "buzzes for one second by default" do
-    GrovePi.Buzzer.buzz(@pin, @prefix)
+  test "buzzes for one second by default",
+    %{prefix: prefix, board: board} do
+    GrovePi.Buzzer.buzz(@pin, prefix)
     Process.sleep 1010
-    {off_time, <<2, @pin, @off, 0>>} = GrovePi.I2C.get_last_write(@board, include_time: true)
-    {on_time, <<2, @pin, @on, 0>>} = GrovePi.I2C.get_last_write(@board, include_time: true)
+    {off_time, <<2, @pin, @off, 0>>} = GrovePi.I2C.get_last_write(board, include_time: true)
+    {on_time, <<2, @pin, @on, 0>>} = GrovePi.I2C.get_last_write(board, include_time: true)
 
     assert_in_delta (off_time - on_time), 1000, 10
   end
 
-  test "buzzes for time specified" do
+  test "buzzes for time specified",
+    %{prefix: prefix, board: board} do
     interval = 1
-    GrovePi.Buzzer.buzz(@pin, interval, @prefix)
+    GrovePi.Buzzer.buzz(@pin, interval, prefix)
     Process.sleep interval + 10
-    {off_time, <<2, @pin, @off, 0>>} = GrovePi.I2C.get_last_write(@board, include_time: true)
-    {on_time, <<2, @pin, @on, 0>>} = GrovePi.I2C.get_last_write(@board, include_time: true)
+    {off_time, <<2, @pin, @off, 0>>} = GrovePi.I2C.get_last_write(board, include_time: true)
+    {on_time, <<2, @pin, @on, 0>>} = GrovePi.I2C.get_last_write(board, include_time: true)
 
     assert_in_delta (off_time - on_time), interval, 10
   end
