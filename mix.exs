ae302db894162f6b649cd28fd161efdb281645cd
Amos L King <amos@binarynoggin.com>
2017-04-28 21:08:05 -0500

    Version bump to 0.3.1
    Amos King @adkron <amos@binarynoggin.com>

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule GrovePi.Mixfile do
   use Mix.Project
 
   @name      "GrovePi"
-  @version   "0.3.0"
+  @version   "0.3.1"
   @github    "https://github.com/fhunleth/grovepi"
   @homepage  @github
 
