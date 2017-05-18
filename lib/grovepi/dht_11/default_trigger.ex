6e86fbe94c36219394e8b07fffb0ef9db0eadc71
Axel Clark <axelclark2@yahoo.com>
2017-05-17 20:42:46 -0400

    Add poller for DHT11 sensor
    Add poller support for a DHT11 sensor.  An event is created for any
    change to the humidity or temperature values.  The DHT11 is read using
    code copied from the DHT module (the DHT module must be refactored for
    polling). The module will only work with DHT module_type 0 which
    is the configuration for the DHT sensor that comes with the GrovePi+
    Starter Kit.

---
+++ lib/grovepi/dht_11/default_trigger.ex
@@ -0,0 +1,39 @@
+defmodule GrovePi.DHT11.DefaultTrigger do
+  @behaviour GrovePi.Trigger
+
+  @moduledoc """
+  This is the default triggering mechanism for DHT11 events. The
+  event is `:changed` and includes the trigger state. The trigger state
+  for the default trigger is a struct containing `temp` and `humidity`
+  properties.
+
+  ## Examples
+      iex> GrovePi.DHT11.DefaultTrigger.init([])
+      {:ok, %GrovePi.DHT11.DefaultTrigger.State{temp: 0, humidity: 0}}
+      iex> GrovePi.DHT11.DefaultTrigger.update({0, 0}, %{temp: 0, humidity: 0})
+      {:ok, %{temp: 0, humidity: 0}}
+      iex> GrovePi.DHT11.DefaultTrigger.update({11.3, 45.5}, %{temp: 0, humidity: 0})
+      {:changed, %{temp: 11.3, humidity: 45.5}}
+      iex> GrovePi.DHT11.DefaultTrigger.update({11.3, 45.5}, %{temp: 11.3, humidity: 45.5})
+      {:ok, %{temp: 11.3, humidity: 45.5}}
+      iex> GrovePi.DHT11.DefaultTrigger.update({22.5, 34.5}, %{temp: 11.3, humidity: 45.5})
+      {:changed, %{temp: 22.5, humidity: 34.5}}
+  """
+
+  defmodule State do
+    @moduledoc false
+    defstruct temp: 0, humidity: 0
+  end
+
+  def init(_) do
+    {:ok, %State{}}
+  end
+
+  def update({temp, humidity}, %{temp: temp, humidity: humidity} = state) do
+    {:ok, state}
+  end
+
+  def update({new_temp, new_humidity}, state) do
+    {:changed, %{state | temp: new_temp, humidity: new_humidity}}
+  end
+end
---
+++ test/grovepi/dht_11/default_trigger_test.exs
@@ -0,0 +1,4 @@
+defmodule GrovePi.DHT11.DefaultTriggerTest do
+  use ExUnit.Case, async: true
+  doctest GrovePi.DHT11.DefaultTrigger
+end
